3a0279994599c97d96af50f59f20f2f5d3d690fd
annafiguerolaruiz <91455277+annafiguerolaruiz@users.noreply.github.com>
2022-03-17 14:01:01 +0100

    sdsdf

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public int CurrentLive;
+    private int Daño = 1;
+    private int Vida = 8;
+    
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //CurrentLive = Vida;
+    }
+
+    public void OnCollissionEnter(Collider otherCollider)
+    {
+        //Cuando la bala colisiona contra el enemigo, el mismo, va perdiendo vida
+        if(gameObject && otherCollider.gameObject.CompareTag("Bala"))
+        {
+            //HacerDaño(1);
+            CurrentLive--;
+            Debug.Log(CurrentLive);
+            Destroy(otherCollider.gameObject);
+
+
+            //Cuando la vida del enemigo llega a 0 , este desaparece
+            if (CurrentLive == 0)
+            {
+                print("hola");
+               
+                Destroy(gameObject);
+
+            }
+        }
+    }
+
+    void HacerDaño( int damage)
+    {
+        CurrentLive -= damage;
     }
 }
